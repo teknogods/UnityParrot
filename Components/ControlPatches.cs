3f841b710971858a4078e49aad1162f518089772
avail <avail@pomf.se>
2020-03-07 14:23:42 +0100

    forgot controls

--- Components/ControlPatches.cs
+++ Components/ControlPatches.cs
@@ -1,4 +1,5 @@
 ﻿using MU3;
+using UnityEngine;
 
 namespace UnityParrot.Components
 {
@@ -7,30 +8,66 @@ namespace UnityParrot.Components
         public static void Patch()
         { 
             Harmony.PatchAllInType(typeof(ControlPatches));
+
+            Harmony.MakeRET(typeof(MU3.Sys.Config), "get_isKeyboardInput", true);
+            Harmony.MakeRET(typeof(MU3.Sys.Config), "get_isKeyboardDebug", true);
         }
 
-        [MethodPatch(PatchType.Prefix, typeof(MU3.Sys.Config), "get_isKeyboardInput")]
-        [MethodPatch(PatchType.Prefix, typeof(MU3.Sys.Config), "get_isKeyboardDebug")]
-        private static bool ReturnTrueDontContinue(ref bool __result)
+        [MethodPatch(PatchType.Prefix, typeof(GameDeviceManager), "isButton")]
+        private static bool IsButton(GameDeviceManager.GKey __0)
         {
-            __result = true;
-            return false;
-        }
-
-        static bool testModeHit = false;
-
-        //[MethodPatch(PatchType.Prefix, typeof(UIInput), "getTriggerOn")]
-        private static bool GetTrigger(ref bool __result, UIInput.Key __0, bool __1)
-        {
-            if (__0 == UIInput.Key.Test && !testModeHit)
+            switch (__0)
             {
-                NekoClient.Logging.Log.Info("yeet");
+                case GameDeviceManager.GKey.WestL:
+                    return Input.GetKeyDown(KeyCode.W);
+                case GameDeviceManager.GKey.WestC:
+                    return Input.GetKeyDown(KeyCode.E);
+                case GameDeviceManager.GKey.WestR:
+                    return Input.GetKeyDown(KeyCode.R);
+                case GameDeviceManager.GKey.EastL:
+                    return Input.GetKeyDown(KeyCode.S);
+                case GameDeviceManager.GKey.EastC:
+                    return Input.GetKeyDown(KeyCode.D);
+                case GameDeviceManager.GKey.EastR:
+                    return Input.GetKeyDown(KeyCode.F);
+                case GameDeviceManager.GKey.WestK:
+                    return Input.GetKeyDown(KeyCode.Q);
+                case GameDeviceManager.GKey.EastK:
+                    return Input.GetKeyDown(KeyCode.T);
+                case GameDeviceManager.GKey.WestM:
+                    return Input.GetKeyDown(KeyCode.LeftArrow);
+                case GameDeviceManager.GKey.EastM:
+                    return Input.GetKeyDown(KeyCode.RightArrow);
+            }
 
-                testModeHit = true;
+            return true;
+        }
 
-                __result = true;
-
-                return false;
+        [MethodPatch(PatchType.Prefix, typeof(GameDeviceManager), "isButtonHold")]
+        private static bool IsButtonHold(GameDeviceManager.GKey __0)
+        {
+            switch (__0)
+            {
+                case GameDeviceManager.GKey.WestL:
+                    return Input.GetKey(KeyCode.W);
+                case GameDeviceManager.GKey.WestC:
+                    return Input.GetKey(KeyCode.E);
+                case GameDeviceManager.GKey.WestR:
+                    return Input.GetKey(KeyCode.R);
+                case GameDeviceManager.GKey.EastL:
+                    return Input.GetKey(KeyCode.S);
+                case GameDeviceManager.GKey.EastC:
+                    return Input.GetKey(KeyCode.D);
+                case GameDeviceManager.GKey.EastR:
+                    return Input.GetKey(KeyCode.F);
+                case GameDeviceManager.GKey.WestK:
+                    return Input.GetKey(KeyCode.Q);
+                case GameDeviceManager.GKey.EastK:
+                    return Input.GetKey(KeyCode.T);
+                case GameDeviceManager.GKey.WestM:
+                    return Input.GetKey(KeyCode.LeftArrow);
+                case GameDeviceManager.GKey.EastM:
+                    return Input.GetKey(KeyCode.RightArrow);
             }
 
             return true;
