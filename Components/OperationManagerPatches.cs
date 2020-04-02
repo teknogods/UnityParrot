1ee263edc496dff29e78f4d2eb566ebe2af02f3d
flame <flame@flame.com>
2020-04-03 05:12:49 +1000

    Remove play restriction due to maintenance or closing for real.

--- Components/OperationManagerPatches.cs
+++ Components/OperationManagerPatches.cs
@@ -10,8 +10,30 @@ namespace UnityParrot.Components
     {
         public static void Patch()
         {
+            Harmony.PatchAllInType(typeof(OperationManagerPatches));
             Harmony.MakeRET(typeof(OperationManager), "isAimeOffline");
             Harmony.MakeRET(typeof(OperationManager), "isAimeLoginDisable");
+            Harmony.MakeRET(typeof(OperationManager), "isAutoRebootNeeded");
+            Harmony.MakeRET(typeof(OperationManager), "isClosed");
+            Harmony.MakeRET(typeof(OperationManager), "isCoinAcceptable", true);
+            Harmony.MakeRET(typeof(OperationManager), "isLoginDisable");
+            Harmony.MakeRET(typeof(OperationManager), "isRebootNeeded");
+            Harmony.MakeRET(typeof(OperationManager), "isShowClosingRemainingMinutes");
+            Harmony.MakeRET(typeof(OperationManager), "isUnderServerMaintenance");
+        }
+
+        [MethodPatch(PatchType.Prefix, typeof(OperationManager), "getCreditUseRestrictionByClose")]
+        static bool getCreditUseRestrictionByClose(ref ClosingManager.CreditUseRestriction __result)
+        {
+            __result = ClosingManager.CreditUseRestriction.None;
+            return false;
+        }
+
+        [MethodPatch(PatchType.Prefix, typeof(OperationManager), "getCreditUseRestrictionByMaintenance")]
+        static bool getCreditUseRestrictionByMaintenance(ref ClosingManager.CreditUseRestriction __result)
+        {
+            __result = ClosingManager.CreditUseRestriction.None;
+            return false;
         }
     }
 }
